e5e26b9dc85217de9cd9deec370fa2226da06af1
Mohammadmehdi <mm.khani@gmail.com>
2024-06-07 23:38:33 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        Welcome to game hub!
+      </div>
+    </>
+  )
+}
+
+export default App
