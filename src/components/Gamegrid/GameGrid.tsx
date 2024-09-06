02360929063e77e2c08ba8ed76caf85bb76c0516
MohammadmehdiKhani <mm.y.khani@gmail.com>
2024-09-06 20:03:24 +0330

    adding infinite scrolling

--- src/components/Gamegrid/GameGrid.tsx
+++ src/components/Gamegrid/GameGrid.tsx
@@ -4,16 +4,19 @@ import useGames from "../../hooks/useGames";
 import GameCardSkeleton from "../GameCardSkeleton/GameCardSkeleton";
 import React from "react";
 import { gameQuery } from "../../App";
-import { Button } from "@mui/material";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 interface Props {
   gameQuery: gameQuery;
 }
 
 export default function GameGrid({ gameQuery }: Props) {
-  const { data, isLoading, isFetchingNextPage, fetchNextPage, hasNextPage } =
-    useGames(gameQuery);
+  const { data, isLoading, fetchNextPage, hasNextPage } =
+  useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  const fetchedGamesSoFar =
+    data?.pages.reduce((total, page) => total + page.results.length, 0) || 0;
 
   return (
     <React.Fragment>
@@ -26,24 +29,22 @@ export default function GameGrid({ gameQuery }: Props) {
       )}
 
       {!isLoading && (
-        <div className={styles.gameGrid}>
-          {data?.pages.map((page, index) => (
-            <React.Fragment key={index}>
-              {page.results.map((game) => (
-                <GameCard game={game}></GameCard>
-              ))}
-            </React.Fragment>
-          ))}
-        </div>
-      )}
-      {hasNextPage && (
-        <Button
-          onClick={() => fetchNextPage()}
-          variant="outlined"
-          sx={{ marginTop: 1 }}
+        <InfiniteScroll
+          dataLength={fetchedGamesSoFar}
+          next={fetchNextPage}
+          hasMore={!!hasNextPage}
+          loader={<h4>Loading...</h4>}
         >
-          {isFetchingNextPage ? "Loading ..." : "Load More"}
-        </Button>
+          <div className={styles.gameGrid}>
+            {data?.pages.map((page, index) => (
+              <React.Fragment key={index}>
+                {page.results.map((game) => (
+                  <GameCard game={game}></GameCard>
+                ))}
+              </React.Fragment>
+            ))}
+          </div>
+        </InfiniteScroll>
       )}
     </React.Fragment>
   );
